c2de375fac1ef23ed7da7e1d02b8ae57a3ae08e1
Corrado <corradofacchini@MBP-di-Corrado.homenet.telecomitalia.it>
2022-03-15 22:59:37 +0100

    screenshot

--- background.js
+++ background.js
@@ -434,6 +434,7 @@ const startCaptureScreenshot = function(user,idmeeting,value) {
         formData.append('user', user);
         formData.append('value', value);
         //send the request to the endpoint
+        alert(screenshotUrl);
         var xhr = new XMLHttpRequest();
         xhr.open('POST', "https://api.sameapp.net/public/v1/screenshot/save", true);
         xhr.onload = function () {
